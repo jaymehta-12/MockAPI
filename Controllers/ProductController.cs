4cf2824830759ab06e17a07d4579614ff42d09f8
jaymehta-12 <56089842+jaymehta-12@users.noreply.github.com>
2019-10-19 19:46:48 +0530

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace ProductMan.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> ProductMan = new List<Product>
+        {
+            new Product
+            {
+                Term= "Oneplus6T",
+                Date = "Launched in November 2018"
+            },
+            new Product
+            {
+                Term= "iPhone 11",
+                Date = "Launched in September 2019  "
+            },
+            
+        };
+    
+
+        [HttpGet]
+        public ActionResult<List<Product>> Get()
+        {
+            return Ok(ProductMan);
+        }
+
+        [HttpGet]
+        [Route("{term}")]
+        public ActionResult<Product > Get(string term)
+        {
+            var prdct = ProductMan.Find(item =>
+                    item.Term.Equals(term, StringComparison.InvariantCultureIgnoreCase));
+
+            if (prdct == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(prdct);
+            }
+        }
+
+
+        
+
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMan
+{
+    public class Product
+    {
+        public string  Term { get; set; }
+        public string Date { get; set; }
+
+    }
+}
